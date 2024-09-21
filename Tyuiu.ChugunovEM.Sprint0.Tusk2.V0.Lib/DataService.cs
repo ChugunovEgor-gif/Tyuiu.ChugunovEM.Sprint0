ba3a0721aed7373727ec4ab8e717b828c4939316
Tyuiu.chugunovEM@outlook.com <Tyuiu.chugunovEM@outlook.com>
2024-09-21 19:30:13 +0300

    Tusk2

---
+++ Tyuiu.ChugunovEM.Sprint0.Tusk2.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ChugunovEM.Sprint0.Tusk2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(String name)
+        {
+            return $"Привет..., {name}";
+        }
+    }
+}
---
+++ Tyuiu.ChugunovEM.Sprint0.Tusk2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChugunovEM.Sprint0.Tusk2.V0.Lib;
+
+namespace Tyuiu.ChugunovEM.Sprint0.Tusk2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //Область создания методов тестирования, методов из библиотеки
+            var name = "Егор";
+            var res = Lib.DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Егор", res);
+        }
+    }
+}
